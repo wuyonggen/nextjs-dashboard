7b1c3acacffb30356057fea3e7292db6b3d041c0
Wu Yonggen <92631388+wuyonggen@users.noreply.github.com>
2026-09-12 20:05:29 +0800

    feat: implement invoice creation and editing functionality with form handling

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,5 +1,6 @@
-import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+import { deleteInvoice } from "@/app/lib/actions";
+import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export function CreateInvoice() {
   return (
@@ -25,13 +26,16 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
   return (
-    <button
-      type="submit"
-      className="rounded-md border border-gray-200 p-2 text-gray-600 transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
-    >
-      <span className="sr-only">Delete</span>
-      <TrashIcon className="size-5" />
-    </button>
+    <form action={deleteInvoiceWithId}>
+      <button
+        type="submit"
+        className="rounded-md border border-gray-200 p-2 text-gray-600 transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
+      >
+        <span className="sr-only">Delete</span>
+        <TrashIcon className="size-5" />
+      </button>
+    </form>
   );
 }
